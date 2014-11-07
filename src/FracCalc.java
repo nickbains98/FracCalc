69ecedf8dfa83c8752f3ec4c9dd03a6f59c7e344
Nicholas <arcadekid14@gmail.com>
2014-11-06 20:54:40 -0800

    cHeCkPoInT 2 11/6/2014
    POOOOOOOOOOOOOOP

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -10,18 +10,47 @@ public class FracCalc {
 			if(input.equals("quit")){
 				p("ok bye");
 				break;
-			}else if(){
-				String firstPart = input.substring(0 , input.indexOf(" "));
-				String secondPart = input.substring(input.indexOf(" "), input.length());
-				int firstNumber = firstPart.nextInt();
-				System.out.println(firstPart + " + " + secondPart);
 			}else{
-				p("Give me a number.");
+				String operator = input.substring(input.indexOf(" "), input.indexOf(" ")+2);
+				String firstPart = input.substring(0 , input.indexOf(" "));
+				String secondPart = input.substring(input.indexOf(operator)+3, input.length());
+				
+				//converts firstPart from a Mixed Number to an improper Fraction
+				String firstPartNotMixed = mixedNumberToImproper(firstPart);
+				String secondPartNotMixed = mixedNumberToImproper(secondPart);
+				
+				System.out.println("You asked me to calculate " + firstPartNotMixed + operator + " " + secondPartNotMixed);
 			}
 		}
 	}
-	
+
 	public static void p(String s){
 		System.out.println(s);
+	}
+	
+	//converts firstPart from a Mixed Number to an improper Fraction
+	public static String mixedNumberToImproper(String s){
+		if (s.contains("_")){
+			int firstUnderscorePosition = s.indexOf("_");
+			
+			//first number before fraction
+			String firstMixedNumberString = s.substring(0 , firstUnderscorePosition);
+			int firstMixedNumber = Integer.parseInt(firstMixedNumberString);
+			
+			//numerator for first number
+			String firstNumeratorString = s.substring(firstUnderscorePosition + 1, s.indexOf("/"));
+			int firstNumerator = Integer.parseInt(firstNumeratorString);
+			
+			//denominator for first number
+			String firstDenominatorString = s.substring(s.indexOf("/")+1 , s.length());
+			int firstDenominator = Integer.parseInt(firstDenominatorString);
+			
+			//Multiplies the whole number portion by the denominator and changes the "firstPart" so it is an improper fraction 
+			firstNumerator = ((firstMixedNumber * firstDenominator) + firstNumerator);
+			s = (firstNumerator + "/" + firstDenominator);
+			return s;
+		}else{
+			return s;
+		}
 	}
 }
