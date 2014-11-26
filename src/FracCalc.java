93a1d77366f3e17f02de36973ac19883532880a3
Nicholas <arcadekid14@gmail.com>
2014-11-25 22:04:20 -0800

    fracCULC good dun!@ erroc handlesd
    It does all the maths and all operationsfefsef it even handlsefs morst
    erars.

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -2,89 +2,268 @@ import java.util.Scanner;
 
 public class FracCalc {	
 	
+	public static int productNumerator = 0;
+	public static int productDenominator = 0;
+	public static int productWholePart = 0;
+	public static boolean error = false;
+	
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		p("I'm FracCalc, type expressions with fractions and I will evaluate them.");
 		p("\"quit\" to exit.");
 		while(true){
+			error = false;
 			String input = scanner.nextLine();
 			if(input.toLowerCase().contains("quit")){
 				p("Thanks for using FracCalc!");
+				scanner.close();
 				break;
 			}else{
-				String operator = input.substring(input.indexOf(" ")+1, input.indexOf(" ")+2);
+				String operator = "";
+				String firstPart = "";
+				String secondPart = "";
+				if(input.indexOf(" ") != -1){
+					operator = input.substring(input.indexOf(" ")+1, input.indexOf(" ")+2);
+					
+					firstPart = input.substring(0 , input.indexOf(" "));
+					
+					secondPart = input.substring(input.indexOf(" ")+3, input.length());
+				}else{
+					error = true;				
+				}
+				int firstNumerator = 0;
+				int firstDenominator = 0;
+				int secondNumerator = 0;
+				int secondDenominator = 0;
 				
-				String firstPart = input.substring(0 , input.indexOf(" "));
 				
-				String secondPart = input.substring(input.indexOf(operator)+2, input.length());
+				//Finds parts of an inputed mixed number
+				if (firstPart.contains("_")){
+					//finds first number before underscore
+					int firstUnderscorePosition = firstPart.indexOf("_");				
+					int firstWholeNumber = firstMixedNumber(firstPart, firstUnderscorePosition);
+					
+					//finds first denominator
+					firstDenominator = denominator(firstPart);
+					
+					//finds first numerator
+					firstNumerator = numerator(firstPart, firstUnderscorePosition, firstWholeNumber, firstDenominator);
+					
+					//(below) if firstPart does not contain a whole number part, but still is a fraction
+				}else if(firstPart.contains("/")){
+					
+					//finds first numerator
+					firstNumerator = fractionNumerator(firstPart);
+					
+					//finds first Denominator
+					firstDenominator = fractionDenominator(firstPart);
+				}else {
+					
+					firstNumerator = parsing(firstPart);
+					firstDenominator = 1;
+				}
 				
-				
-				//converts Parts from Mixed Numbers to an improper Fractions
-				String firstPartNotMixed = mixedNumberToImproper(firstPart);
-				String secondPartNotMixed = mixedNumberToImproper(secondPart);				
+				//does same as above, but with second part
+				if (secondPart.contains("_")){
+					int secondUnderscorePosition = secondPart.indexOf("_");
+					//---------------------------------------------finds values of secondPart
+					int secondWholeNumber = firstMixedNumber(secondPart, secondUnderscorePosition);				
+					
+					//finds second denominator
+					secondDenominator = denominator(secondPart);
+					
+					//finds second numerator
+					secondNumerator = numerator(secondPart, secondUnderscorePosition, secondWholeNumber, secondDenominator);
+					//---------------------------------------------finds values of secondPart
+					
+					secondPart = mixedNumberToImproperFraction(secondWholeNumber, secondNumerator, secondDenominator);
+					
+				}else if(secondPart.contains("/")){
+					
+					//finds second numerator
+					secondNumerator = fractionNumerator(secondPart);
+					
+					//finds second Denominator
+					secondDenominator = fractionDenominator(secondPart);
+				}else {
+					
+					secondNumerator = parsing(secondPart);
+					secondDenominator = 1;
+				}
 				
 				//checks which operator the user inputed and completes equation
-				String product = "";
+				
 				if (operator.equals("+")){
-					 product = addition(firstPartNotMixed, secondPartNotMixed);					
-					 
+					addition(firstNumerator, firstDenominator, secondNumerator, secondDenominator);
+					
+				}else if(operator.equals("-")){
+					subtraction(firstNumerator, firstDenominator, secondNumerator, secondDenominator);
+					
+				}else if(operator.equals("*")){
+					multiplication(firstNumerator, firstDenominator, secondNumerator, secondDenominator);
+					
+				}else if(operator.equals("/")){
+					division(firstNumerator, firstDenominator, secondNumerator, secondDenominator);
+					
+				}else{
+					p("not a valid operator");					
 				}
-				System.out.println(product);
+				reduce();
 			}
 		}
-	}
-
+	} 
+	
+	//easy println method
 	public static void p(String s){
 		System.out.println(s);
 	}
 	
-	//converts firstPart from a Mixed Number to an improper Fraction
-	public static String mixedNumberToImproper(String s){
-		if (s.contains("_")){
-			int firstUnderscorePosition = s.indexOf("_");
+	//-----------------------------------------------------------------------------------------Mixed Number Pieces
+	//finds whole number before underscore for either first or second part
+	public static int firstMixedNumber(String s, int UnderscorePosition){
+		String MixedNumberString = s.substring(0 , UnderscorePosition);
+		int MixedNumber = parsing(MixedNumberString);
+		return MixedNumber;
+	}
+	
+	//finds numerator
+	public static int numerator(String s, int underscorePosition, int wholeNumber, int denominator){
+		String firstNumeratorString = s.substring(underscorePosition + 1, s.indexOf("/"));
+		int firstNumerator = parsing(firstNumeratorString);
+		
+		int x = denominator * wholeNumber;
+		
+		firstNumerator += x;
+		return firstNumerator;
+	}
+	
+	//finds denominator
+	public static int denominator(String s){
+		String denominatorString = s.substring(s.indexOf("/")+1 , s.length());
+		int denominator = parsing(denominatorString);
+		return denominator;
+	}
+	//-----------------------------------------------------------------------------------------Mixed Number Pieces
+	
+	//=========================================================================================Fraction Pieces
+	//finds numerator
+	public static int fractionNumerator(String fraction){
+		String numeratorString = fraction.substring(0, fraction.indexOf("/"));
+		int numeratorInt = parsing(numeratorString);
+		return numeratorInt;
+	}
+	
+	public static int fractionDenominator(String fraction){
+		String denominatorString = fraction.substring(fraction.indexOf("/")+1, fraction.length());
+		int denominatorInt = parsing(denominatorString);
+		return denominatorInt;
+	}
+	
+	
+	//=========================================================================================Fraction Pieces
+
+	public static String mixedNumberToImproperFraction(int wholeNumber, int numerator, int denominator){
+		int x = wholeNumber*denominator;
+		numerator = numerator + x;
+		String improperFraction = numerator + "/" + denominator;
+		return improperFraction;
+	}
+	
+	//checks to see if Input is parseable, then parses it if so.
+	public static int parsing(String s){
+		Scanner parser = new Scanner(s);
+		
+		if (parser.hasNextInt()){
 			
-			//first number before fraction
-			String firstMixedNumberString = s.substring(0 , firstUnderscorePosition);
-			int firstMixedNumber = Integer.parseInt(firstMixedNumberString);
+			return parser.nextInt();
 			
-			//numerator for first number
-			String firstNumeratorString = s.substring(firstUnderscorePosition + 1, s.indexOf("/"));
-			int firstNumerator = Integer.parseInt(firstNumeratorString);
-			
-			//denominator for first number
-			String firstDenominatorString = s.substring(s.indexOf("/")+1 , s.length());
-			int firstDenominator = Integer.parseInt(firstDenominatorString);
-			
-			//Multiplies the whole number portion by the denominator and changes the "firstPart" so it is an improper fraction 
-			firstNumerator = ((firstMixedNumber * firstDenominator) + firstNumerator);
-			s = (firstNumerator + "/" + firstDenominator);
-			return s;
+		}
+		
+		error = true;
+		
+		return 1;
+	}
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Operations
+	//it adds bro
+	public static void addition(int firstNumerator, int firstDenominator, int secondNumerator, int secondDenominator){		
+		int newFirstNumerator = firstNumerator * secondDenominator;
+		int newSecondNumerator = secondNumerator * firstDenominator;
+		
+		productDenominator = firstDenominator * secondDenominator;
+		productNumerator = newFirstNumerator + newSecondNumerator;
+	}
+	
+	public static void subtraction(int firstNumerator, int firstDenominator, int secondNumerator, int secondDenominator){
+		int newFirstNumerator = firstNumerator * secondDenominator;
+		int newSecondNumerator = secondNumerator * firstDenominator;
+		
+		productDenominator = firstDenominator * secondDenominator;
+		productNumerator = newFirstNumerator - newSecondNumerator;		
+	}
+	
+	public static void multiplication(int firstNumerator, int firstDenominator, int secondNumerator, int secondDenominator){
+		productDenominator = firstDenominator * secondDenominator;
+		productNumerator = firstNumerator * secondNumerator;	
+	}
+	
+	public static void division(int firstNumerator, int firstDenominator, int secondNumerator, int secondDenominator){
+		productDenominator = firstDenominator * secondNumerator;
+		productNumerator = firstNumerator * secondDenominator;
+	}
+	
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Operations
+	
+	
+	public static void reduce(){
+		
+		int gcdizzle = gcd(productNumerator, productDenominator);
+		if (!error){
+			productNumerator /= gcdizzle;
+			productDenominator /= gcdizzle;
+		}
+		if(productDenominator != 0){
+			productWholePart = productNumerator / productDenominator;
+			productNumerator %= productDenominator;
 		}else{
-			return s + "/1";
+			error = true;
+			
+		}
+		
+		
+		if(productDenominator < 0 && productWholePart == 0){
+			productDenominator *= -1;
+			
+			productNumerator *= -1;
+		}else if (productDenominator < 0){
+			
+			productDenominator *= -1;
+			
+			productNumerator = Math.abs(productNumerator);
+		}
+			
+		if (!error){
+			if(productWholePart == 0 && productNumerator == 0){
+				System.out.println("= 0");
+			}else if(productWholePart == 0){
+				System.out.println("= " + productNumerator + "/" + productDenominator);
+			}else if(productNumerator == 0){		
+				System.out.println("= " + productWholePart);
+			}else{
+				System.out.println("= " + productWholePart + "_" + productNumerator + "/" + productDenominator);		
+			}
+		}else{
+			p("There was totes an error");
+			
 		}
 	}
 	
-	public static String addition(String a, String b){
-		String firstDenominatorString = a.substring(a.indexOf("/")+1 , a.length());
-		int firstDenominator = Integer.parseInt(firstDenominatorString);
-		
-		String secondDenominatorString = b.substring(b.indexOf("/")+1 , b.length());
-		int secondDenominator = Integer.parseInt(secondDenominatorString);
-		
-		int newDenominator = firstDenominator * secondDenominator;
-		
-		String firstNumeratorString = a.substring(0, a.indexOf("/"));
-		int firstNumerator = Integer.parseInt(firstNumeratorString);
-		
-		String secondNumeratorString = b.substring(0, b.indexOf("/"));
-		int secondNumerator = Integer.parseInt(secondNumeratorString);
-		
-		int newFirstNumerator = firstNumerator*secondDenominator;
-		int newSecondNumerator = secondNumerator*firstDenominator;
-		
-		int newFinalNumerator= newSecondNumerator + newFirstNumerator;
-		
-		return "" + newFinalNumerator + "/" + newDenominator;
+	public static int gcd(int a, int b){
+		int t;
+		while (b != 0) {
+			t = b;
+			b = a % b;
+			a = t;			
+		}
+		return a;
 	}
-	
 }
